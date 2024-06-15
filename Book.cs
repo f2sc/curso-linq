be1951d3f392113e4931fde579dff7af69104f32
Fabio <fabio@f2sc.com>
2024-06-15 11:22:57 +0200

    Feat: Añadido json con datos de libros. Añadida clase Book.cs para "mapear" cada libro. Añadida la clase LinqQueries donde vamos a escribir el código con LinQ

---
+++ Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_linq
+{
+    public class Book
+    {
+        private string Title {get; set;}
+
+        private int PageCount {get; set;}
+
+        private DateTime PublishedDate {get; set;}
+
+        private string ThumbnailUrl {get; set;}
+
+        private string ShortDescription {get; set;}
+
+        private status Status {get; set;}
+
+        private string[] Authors {get; set;}
+
+        private string[] Categories {get; set;}
+
+
+    }
+
+    public enum status
+    {
+        PUBLISH,
+        UNPUBLISH,
+    }
+}
---
+++ LinqQueries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_linq
+{
+    public class LinqQueries
+    {
+        public LinqQueries() { 
+
+        }
+    }
+}
